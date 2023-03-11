025cfeb660b63229bc047f18d90a0e2ff5709daa
lewis-dbi <lewis@deliciousbrains.com>
2023-03-11 01:15:18 +0000

    revert vite.config changes

--- vite.config.js
+++ vite.config.js
@@ -8,24 +8,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    resolve: {
-        alias: {
-          '@': '/src',
-        },
-        extensions: ['.svg'], // Add .svg extension here
-    },
-    optimizeDeps: {
-        include: ['@iconify/iconify'],
-    },
-    build: {
-        assetsInlineLimit: 0,
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    // Define chunk for SVG files
-                    'assets/svg/*.svg': ['svg'],
-                },
-            },
-        },
-    },
 });
