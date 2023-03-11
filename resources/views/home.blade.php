a6958e858ec7b1df0dec839ad3361614f0ce5811
lewis-dbi <lewis@deliciousbrains.com>
2023-03-11 01:47:26 +0000

    add width and height to images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,7 +36,7 @@
                     <!-- button -->
                     <div class="h-48 p-1 border border-white/12 w-max items-center rounded-full">
                         <a href="https://www.notion.so/Lewis-Warren-e1d45d2cedc940269a57283ee9fccbb5" target="_blank" class="text-14 text-white/80 font-light bg-white/2 rounded-full h-full px-16 inline-flex items-center shadow-2xl hover:bg-white/4 hover:shadow-purple-500/40">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none">
+                        <svg class="mr-12" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none">
                             <path fill="url(#paint0_linear_105_1181)" d="M9.75 1.695v3.097c0 .675.008.833.158 1.125.142.278.367.51.652.653.293.15.45.157 1.125.157h3.098c.412 0 .75-.337.75-.75 0-.42-.338-.75-.75-.75h-3.105c-.375 0-.465-.007-.458 0l-.007-.007V1.665c0-.42-.338-.75-.75-.75-.42 0-.75.33-.75.75l.037.03Zm.75 10.297H6c-.42 0-.75.33-.75.75 0 .413.33.75.75.75h4.5c.413 0 .75-.337.75-.75 0-.42-.337-.75-.75-.75Zm1.5-3H6c-.42 0-.75.33-.75.75 0 .413.33.75.75.75h6c.413 0 .75-.337.75-.75 0-.42-.337-.75-.75-.75Zm2.25-1.515v5.408c0 1.418-.03 1.763-.165 2.025a1.54 1.54 0 0 1-.66.652c-.27.136-.607.158-2.032.158h-4.8c-1.425 0-1.77-.03-2.033-.165a1.517 1.517 0 0 1-.66-.66c-.142-.27-.165-.607-.165-2.032v-7.8c0-1.426.023-1.77.158-2.033.142-.285.367-.518.652-.66.263-.143.6-.165 2.025-.165h2.408c.615 0 .757 0 .907.037.15.03.293.09.428.173.015.007.03.022.052.03.113.075.225.18.615.577l2.385 2.385c.435.435.533.54.608.668.075.127.142.277.172.427.03.143.038.285.038.908l.067.067Zm1.5 0c0-.757-.015-.952-.09-1.26a3.05 3.05 0 0 0-.36-.87c-.172-.277-.3-.42-.832-.952l-2.393-2.393c-.465-.465-.6-.592-.817-.742-.053-.038-.098-.068-.143-.098a2.85 2.85 0 0 0-.87-.36c-.315-.075-.502-.09-1.26-.09H6.57c-1.732 0-2.137.03-2.715.323A2.946 2.946 0 0 0 2.543 2.34c-.3.57-.33.982-.33 2.707v7.8c0 1.726.03 2.13.322 2.708.285.563.743 1.02 1.305 1.305.57.293.983.323 2.708.323h4.8c1.725 0 2.13-.038 2.707-.33a3.022 3.022 0 0 0 1.305-1.313c.293-.577.323-.99.323-2.715V7.41l.067.067Z"/>
                             <defs>
                                 <linearGradient id="paint0_linear_105_1181" x1="2.213" x2="15.747" y1="8.947" y2="8.721" gradientUnits="userSpaceOnUse">
@@ -51,7 +51,7 @@
                     <!-- button -->
                     <div class="h-48 p-1 bg-gradient-to-b from-white/20 to-white/8 w-max items-center rounded-full shadow-default">
                         <a href="https://form.typeform.com/to/fObUcyQl" class="text-14 text-white/90 font-light rounded-full h-full bg-[#25222A] px-16 inline-flex items-center shadow-2xl hover:bg-[#2D2934] hover:shadow-blue/40 transition">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none">
+                        <svg class="mr-12" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none">
                             <path fill="url(#paint0_linear_105_1173)" d="M5.625 9.75h.008c.412 0 .75-.338.75-.75 0-.42-.338-.75-.75-.75h-.008c-.42 0-.75.33-.75.75 0 .412.33.75.75.75ZM9 9.75h.008c.412 0 .75-.338.75-.75 0-.42-.338-.75-.75-.75H9c-.42 0-.75.33-.75.75 0 .412.33.75.75.75Zm3.375 0h.008c.412 0 .75-.338.75-.75 0-.42-.338-.75-.75-.75h-.008c-.42 0-.75.33-.75.75 0 .412.33.75.75.75ZM9 16.5c4.14 0 7.5-3.36 7.5-7.5 0-4.147-3.36-7.5-7.5-7.5A7.493 7.493 0 0 0 1.5 9c0 .975.188 1.928.547 2.82.053.127.068.172.068.165l-.008-.008c-.007-.007-.007.015-.03.136l-.45 2.662c-.097.54-.105.652.023.938.113.262.323.472.585.585.285.12.39.112.938.022l2.662-.45a.883.883 0 0 0 .135-.03c-.007-.008.03.008.165.068.885.36 1.838.547 2.82.547L9 16.5ZM9 15a6.024 6.024 0 0 1-2.258-.443c-.247-.104-.292-.12-.404-.142a1.274 1.274 0 0 0-.33-.037c-.12 0-.173.007-.39.037l-2.67.443a4.71 4.71 0 0 0-.263.037c.022-.008.075 0 .157.037a.38.38 0 0 1 .196.195c.03.075.037.128.037.158 0-.045.007-.098.037-.262l.443-2.67c.03-.226.037-.278.037-.39a2.337 2.337 0 0 0-.037-.33 2.437 2.437 0 0 0-.143-.405A5.998 5.998 0 0 1 8.97 2.97c3.307-.001 6 2.684 6 5.999 0 3.307-2.693 6-6 6L9 15ZM5.25 9c0-.21.165-.375.375-.375.202 0 .375.165.375.375a.381.381 0 0 1-.375.375A.376.376 0 0 1 5.25 9Zm1.5 0c0-.623-.51-1.125-1.125-1.125C5.003 7.875 4.5 8.377 4.5 9c0 .615.503 1.125 1.125 1.125.615 0 1.125-.51 1.125-1.125Zm1.875 0c0-.21.165-.375.375-.375.203 0 .375.165.375.375A.381.381 0 0 1 9 9.375.376.376 0 0 1 8.625 9Zm1.5 0c0-.623-.51-1.125-1.125-1.125-.623 0-1.125.502-1.125 1.125 0 .615.502 1.125 1.125 1.125.615 0 1.125-.51 1.125-1.125ZM12 9c0-.21.165-.375.375-.375.203 0 .375.165.375.375a.381.381 0 0 1-.375.375A.376.376 0 0 1 12 9Zm1.5 0c0-.623-.51-1.125-1.125-1.125-.623 0-1.125.502-1.125 1.125 0 .615.502 1.125 1.125 1.125.615 0 1.125-.51 1.125-1.125Z"/>
                             <defs>
                                 <linearGradient id="paint0_linear_105_1173" x1="1.5" x2="16.494" y1="9" y2="8.695" gradientUnits="userSpaceOnUse">
@@ -192,7 +192,7 @@
             <div class="w-full grid grid-cols-1 md:grid-cols-2 gap-64 md:gap-40 lg:gap-64 items-center justify-between">
                 <!-- project -->
                 <a href="https://spinupwp.com" target="_blank" class="w-full gap-24 flex flex-col">
-                    <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/project-spinupwp.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" alt="SpinupWP Project"/>
+                    <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/project-spinupwp.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" alt="SpinupWP Project" width="1120" height="480"/>
                     <div class="flex flex-col gap-16">
                         <div class="w-full flex items-center justify-between">
                             <h3 class="text-18">SpinupWP</h3>
@@ -205,7 +205,7 @@
                 </a>
                  <!-- project -->
                 <a href="https://deploymenthawk.com" target="_blank" class="w-full gap-24 flex flex-col">
-                    <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/project-deploymenthawk.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" alt="DeploymentHawk Project"/>
+                    <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/project-deploymenthawk.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" alt="DeploymentHawk Project" width="1120" height="480"/>
                     <div class="flex flex-col gap-16">
                         <div class="w-full flex items-center justify-between">
                             <h3 class="text-18">DeploymentHawk</h3>
@@ -218,7 +218,7 @@
                 </a>
                  <!-- project -->
                 <div class="w-full gap-24 flex flex-col">
-                    <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/project-game.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" alt="Game Project"/>
+                    <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/project-game.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" alt="Game Project" width="1120" height="480"/>
                     <div class="flex flex-col gap-16">
                         <div class="w-full flex items-center justify-between">
                             <h3 class="text-18">Missing (WIP Title)</h3>
@@ -262,10 +262,10 @@
         <section id="games" class="w-full max-w-fixed m-auto flex flex-col items-center gap-56 my-120 md:my-200 px-16 xl:px-0">
             <h2 class="text-24 sm:text-32 font-medium text-transparent bg-clip-text bg-gradient-to-r from-white/60 to-white/20 tracking-[0.025rem] mr-auto">Lately, I've enjoyed playing...</h2>
             <div class="w-full grid grid-cols-2 md:grid-cols-4 gap-24 sm:gap-40 items-center justify-between">
-                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/game-hollowknight.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="Hollow Knight" alt="Hollow Knight Cover Art"/>
-                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/game-godofwar2.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="God of War: Ragnorak" alt="God of War: Ragnorak Cover Art"/>
-                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/game-reddead2.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="Red Dead Redemption 2" alt="Red Dead Redemption 2 Cover Art"/>
-                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/game-lastofus.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="The Last of Us" alt="The Last of Us Cover Art"/>
+                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/game-hollowknight.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="Hollow Knight" alt="Hollow Knight Cover Art" width="480" height="320"/>
+                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/game-godofwar2.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="God of War: Ragnorak" alt="God of War: Ragnorak Cover Art" width="480" height="320"/>
+                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/game-reddead2.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="Red Dead Redemption 2" alt="Red Dead Redemption 2 Cover Art" width="480" height="320"/>
+                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/game-lastofus.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="The Last of Us" alt="The Last of Us Cover Art" width="480" height="320"/>
             </div>
         </section>
 
@@ -274,12 +274,12 @@
         <section id="albums" class="w-full max-w-fixed m-auto flex flex-col items-center gap-56 my-120 md:my-200 px-16 xl:px-0">
             <h2 class="text-24 sm:text-32 font-medium text-transparent bg-clip-text bg-gradient-to-r from-white/60 to-white/20 tracking-[0.025rem] mr-auto">You’ll often find me listening too...</h2>
             <div class="w-full grid grid-cols-3 md:grid-cols-6 items-center gap-24 sm:gap-40 md:gap-24 lg:gap-40">
-                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/album-madeon.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="Madeon - Adventure Album" alt="Madeon Album Artwork"/>
-                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/album-daftpunk.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="Daft Punk - Alive Album" alt="Daft Punk - Alive Album Artwork"/>
-                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/album-foofighters.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="Foo Fighters - The Colour and the Shape Album" alt="Foo Fighters - The Colour and the Shape Album Artwork"/>
-                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/album-pinkfloyd.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="Pink Floyd - Dark Side of the Moon Album" alt="Pink Floyd - Dark Side of the Moon Album Artwork"/>
-                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/album-fosterthepeople.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="Foster the People - Torches Album" alt="Foster the People - Torches Album Artwork"/>
-                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/album-deadmau5.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="Deadmau5 - Random Album Title" alt="Deadmau5 - Random Album Title Artwork"/>
+                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/album-madeon.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="Madeon - Adventure Album" alt="Madeon Album Artwork" width="320" height="320"/>
+                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/album-daftpunk.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="Daft Punk - Alive Album" alt="Daft Punk - Alive Album Artwork" width="320" height="320"/>
+                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/album-foofighters.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="Foo Fighters - The Colour and the Shape Album" alt="Foo Fighters - The Colour and the Shape Album Artwork" width="320" height="320"/>
+                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/album-pinkfloyd.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="Pink Floyd - Dark Side of the Moon Album" alt="Pink Floyd - Dark Side of the Moon Album Artwork" width="320" height="320"/>
+                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/album-fosterthepeople.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="Foster the People - Torches Album" alt="Foster the People - Torches Album Artwork" width="320" height="320"/>
+                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/album-deadmau5.webp') }}" class="w-full shadow-default rounded-xl border border-white/4" title="Deadmau5 - Random Album Title" alt="Deadmau5 - Random Album Title Artwork" width="320" height="320"/>
             </div>
         </section>
 
@@ -297,7 +297,7 @@
             <!-- button -->
             <div class="h-56 p-1 bg-gradient-to-b from-white/20 to-white/8 w-max items-center rounded-full shadow-default">
                 <a href="https://form.typeform.com/to/fObUcyQl" class="text-18 text-white/90 font-regular rounded-full h-full bg-[#25222A] rounded-full px-24 inline-flex items-center shadow-2xl hover:bg-[#2D2934] hover:shadow-blue/40 transition">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none">
+                <svg class="mr-12" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none">
                     <path fill="url(#paint0_linear_105_1173)" d="M5.625 9.75h.008c.412 0 .75-.338.75-.75 0-.42-.338-.75-.75-.75h-.008c-.42 0-.75.33-.75.75 0 .412.33.75.75.75ZM9 9.75h.008c.412 0 .75-.338.75-.75 0-.42-.338-.75-.75-.75H9c-.42 0-.75.33-.75.75 0 .412.33.75.75.75Zm3.375 0h.008c.412 0 .75-.338.75-.75 0-.42-.338-.75-.75-.75h-.008c-.42 0-.75.33-.75.75 0 .412.33.75.75.75ZM9 16.5c4.14 0 7.5-3.36 7.5-7.5 0-4.147-3.36-7.5-7.5-7.5A7.493 7.493 0 0 0 1.5 9c0 .975.188 1.928.547 2.82.053.127.068.172.068.165l-.008-.008c-.007-.007-.007.015-.03.136l-.45 2.662c-.097.54-.105.652.023.938.113.262.323.472.585.585.285.12.39.112.938.022l2.662-.45a.883.883 0 0 0 .135-.03c-.007-.008.03.008.165.068.885.36 1.838.547 2.82.547L9 16.5ZM9 15a6.024 6.024 0 0 1-2.258-.443c-.247-.104-.292-.12-.404-.142a1.274 1.274 0 0 0-.33-.037c-.12 0-.173.007-.39.037l-2.67.443a4.71 4.71 0 0 0-.263.037c.022-.008.075 0 .157.037a.38.38 0 0 1 .196.195c.03.075.037.128.037.158 0-.045.007-.098.037-.262l.443-2.67c.03-.226.037-.278.037-.39a2.337 2.337 0 0 0-.037-.33 2.437 2.437 0 0 0-.143-.405A5.998 5.998 0 0 1 8.97 2.97c3.307-.001 6 2.684 6 5.999 0 3.307-2.693 6-6 6L9 15ZM5.25 9c0-.21.165-.375.375-.375.202 0 .375.165.375.375a.381.381 0 0 1-.375.375A.376.376 0 0 1 5.25 9Zm1.5 0c0-.623-.51-1.125-1.125-1.125C5.003 7.875 4.5 8.377 4.5 9c0 .615.503 1.125 1.125 1.125.615 0 1.125-.51 1.125-1.125Zm1.875 0c0-.21.165-.375.375-.375.203 0 .375.165.375.375A.381.381 0 0 1 9 9.375.376.376 0 0 1 8.625 9Zm1.5 0c0-.623-.51-1.125-1.125-1.125-.623 0-1.125.502-1.125 1.125 0 .615.502 1.125 1.125 1.125.615 0 1.125-.51 1.125-1.125ZM12 9c0-.21.165-.375.375-.375.203 0 .375.165.375.375a.381.381 0 0 1-.375.375A.376.376 0 0 1 12 9Zm1.5 0c0-.623-.51-1.125-1.125-1.125-.623 0-1.125.502-1.125 1.125 0 .615.502 1.125 1.125 1.125.615 0 1.125-.51 1.125-1.125Z"/>
                     <defs>
                         <linearGradient id="paint0_linear_105_1173" x1="1.5" x2="16.494" y1="9" y2="8.695" gradientUnits="userSpaceOnUse">
@@ -316,8 +316,8 @@
         <footer class="w-full max-w-fixed m-auto flex flex-col items-center pt-80 sm:pt-160 group relative overflow-hidden px-16 xl:px-0">
             <div class="w-max relative flex items-baseline gap-48">
                 <p class="hidden md:block text-24 text-paragraph/50 w-[18rem] text-center absolute right-440 rotate-[-9deg] font-script">My cats are always spying...</p>
-                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/cat-molly.webp') }}" class="h-96 sm:h-[12.4rem] relative group-hover:translate-y-160 transition duration-300 ease-in-out" alt="cat"/>
-                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/cat-bourbon.webp') }}" class="h-96 sm:h-[12.4rem] relative group-hover:translate-y-160 transition duration-700 ease-in-out" alt="cat"/>
+                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/cat-molly.webp') }}" class="h-96 sm:h-[12.4rem] relative group-hover:translate-y-160 transition duration-300 ease-in-out" alt="cat" width="314" height="252"/>
+                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/webp/cat-bourbon.webp') }}" class="h-96 sm:h-[12.4rem] relative group-hover:translate-y-160 transition duration-700 ease-in-out" alt="cat" width="364" height="252"/>
             </div>
         </footer>
         
